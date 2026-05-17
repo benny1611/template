2e1ca94af22d893651874e620b58d1cfc3bd3733
benny1611 <benny_1611@protonmail.com>
2026-05-17 11:15:27 +0200

    added https support for frontend

--- template_frontend/template/vite.config.ts
+++ template_frontend/template/vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr()
+    svgr(),
+    basicSsl()
   ],
   server: {
     // Allow network access
     host: '0.0.0.0',
-    // Optional: specify a port
-    port: 3000
+    port: 5173
   }
 })
